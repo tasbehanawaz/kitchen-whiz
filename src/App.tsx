81828c8460b40db2bcbcc4ae17955ab03f7d70ae
Tasbeha Nawaz <tasbehanawaz@gmail.com>
2023-09-20 12:25:08 +0500

    setup chakra ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
 import React from 'react';
 import './App.css';
+import {Button, ChakraProvider} from '@chakra-ui/react'
+
+function MyApp() {
+    return (
+        <div className="App">
+            <Button colorScheme="blue">Button</Button>
+        </div>
+    )
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <ChakraProvider>
+            <MyApp/>
+        </ChakraProvider>
+    );
 }
 
 export default App;
